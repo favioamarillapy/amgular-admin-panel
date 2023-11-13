821278c872b172344d0d6300ddbd191933f21f87
favio.amarilla <favio.amarilla@konecta.com.py>
2023-11-13 09:23:43 -0300

    Styles changed

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -4,44 +4,45 @@ import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent {
   items: MenuItem[] = [{ label: 'Users' }];
   home: MenuItem = { icon: 'pi pi-home', routerLink: '/' };
 
+  visibleFilters: boolean = false;
   cols: any = [
     { field: 'id', header: '#ID' },
     { field: 'firstName', header: 'First Name' },
-    { field: 'lastName', header: 'Last Name' }
+    { field: 'lastName', header: 'Last Name' },
   ];
 
   users = [
     {
       id: 1,
       firstName: 'John',
-      lastName: 'Doe'
+      lastName: 'Doe',
     },
     {
       id: 2,
       firstName: 'John',
-      lastName: 'Doe'
+      lastName: 'Doe',
     },
     {
       id: 3,
       firstName: 'John',
-      lastName: 'Doe'
+      lastName: 'Doe',
     },
     {
       id: 4,
       firstName: 'John',
-      lastName: 'Doe'
+      lastName: 'Doe',
     },
     {
       id: 5,
       firstName: 'John',
-      lastName: 'Doe'
-    }
+      lastName: 'Doe',
+    },
   ];
 
   first: number = 0;
